2fd26b3c3608fed41948e2bca69ec4448e085ff3
aditya bhojane <bhojaneaditya7@gmail.com>
2024-11-13 19:01:06 +0530

    add vercel dix

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    lib: {
-      entry: 'src/main.jsx', // specify your entry file here
-      name: 'ReactTailwindSetup',
-      fileName: (format) => `react-tailwind-setup.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
+    outDir: 'dist', // Ensure the output is 'dist'
   },
 });
+;
